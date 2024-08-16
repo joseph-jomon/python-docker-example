e41991277743c2ba16083b381da77930377af7f9
joseph-jomon <josephjomon401@gmail.com>
2024-08-16 16:34:26 +0200

    hallo change

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/api")
 async def root():
-    return {"message": "Hello World this is joseph and this is my api endpoint routed with traefik"}
+    return {"message": "Hello World this is joseph and this is my api endpoint routed with traefik, and hey you the one sitiing in traffck schike eine hallo"}
