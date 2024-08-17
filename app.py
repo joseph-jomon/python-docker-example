9bad104480da9eaccf2633b2b9b579fd8986726d
joseph-jomon <josephjomon401@gmail.com>
2024-08-17 14:58:31 +0200

    changed the service name it can be any name

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/api")
 async def root():
-    return {"message": "Hello World this is joseph and this is my api endpoint routed with traefik, and hey you the one sitiing in traffck schike eine hallo"}
+    return {"message": "Hello World this is joseph and this is my Fastapi endpoint routed with traefik from a docker compose file"}
