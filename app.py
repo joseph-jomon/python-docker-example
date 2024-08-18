49499603bf9dc34ee861af2ac18d574d90872d7f
joseph-jomon <josephjomon401@gmail.com>
2024-08-18 19:04:40 +0200

    changed path to path prefix

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/api")
 async def root():
-    return {"message": "Hello World this is joseph and this is my Fastapi endpoint routed with traefik from a docker compose file"}
+    return {"message": "Hello World this is joseph and this is my Fastapi endpoint and routed with traefik from a docker compose file"}
